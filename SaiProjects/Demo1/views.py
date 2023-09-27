78b4af326f3aa2be5df56988f6ff5f0adf665b31
Sai Prasad Vijayakumar <102521517+ItsMeVSP@users.noreply.github.com>
2023-09-27 06:24:00 +0530

    Delete SaiProjects directory

--- SaiProjects/Demo1/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    msg="<h1>Hi Naan Sai</h1>"
-    return HttpResponse(msg)
